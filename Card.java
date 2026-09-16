0d95d0726ce2f82c90fcbebb2e35d13eb2bf988e
AzkaRasool <sp26-bcs-011@cuilahore.edu.pk>
2026-09-16 13:21:33 +0500

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,19 @@
+public class Card{
+
+
+String suit;
+int rank;
+
+
+
+void display(){
+
+System.out.println("Suit:"+suit);
+System.out.println("Rank!!!!:"+rank);
+
+
+}
+
+public static void main(String args[]){
+}
+}
